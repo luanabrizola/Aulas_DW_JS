0c49fc60b2dcf17e46a400808796b8f7131592a7
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2024-06-19 11:17:50 -0300

    Update encadeamento.js

--- aula19/encadeamento.js
+++ aula19/encadeamento.js
@@ -1,34 +1,58 @@
-function pedirPizza(tempo=2000){
+function pedirPizza(tempo = 2000, saborDisponivel = true) {
     console.log("Iniciando o pedido da pizza")
-    const promessa=new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            if (saborDisponivel){
+    const promessa = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (saborDisponivel) {
                 resolve("Pizza A caminho")
             } else {
                 reject("Não será possivel entregar a pizza")
             }
         }, tempo)
-        
+
     })
     return promessa;
 }
 
-function pedirUber(tempo=2000, motoristaDisponivel=true){
+function pedirRefrigerante(tempo = 2000, refriDisponivel = true) {
+    console.log("Iniciando o pedido do refrigeratne")
+    const promessa = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (refriDisponivel) {
+                resolve("Refri A caminho")
+            } else {
+                reject(new Error("Não será possivel entregar o refrigerante"))
+            }
+        }, tempo)
+
+    })
+    return promessa;
+}
+
+function pedirUber(tempo = 2000, motoristaDisponivel = true) {
 
     console.log("Iniciando a solicitação da corrida")
 
-    const promessa=new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            if (motoristaDisponivel){
+    const promessa = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (motoristaDisponivel) {
                 resolve("Motorista a caminho")
             } else {
-                reject( new Error ("Não a motorista disponivel"))
+                reject(new Error("Não a motorista disponivel"))
             }
         }, tempo)
-        
+
     })
     return promessa;
 }
+
+function pedirMaisComida(){
+    Promise.all([pedirPizza(), pedirRefrigerante(10000, false)])
+    .then(respostas=>{
+        console.log(respostas)
+    })
+    .catch(erro=>console.log(erro))
+}
+pedirMaisComida()
 
 
 function irConfraternizacao() {
